f23aeca90a09ed59921e37897803098aec4350fe
Kostadin Urumov <koceurumov@gmail.com>
2024-03-03 13:28:01 +0200

    area added

---
+++ My_Transfermarkt/Areas/Administrator/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My_Transfermarkt.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    [Route("/Administrator/[controller]/[Action]/{id?}")]
+    public class BaseController : Controller
+    {
+        
+    }
+}
