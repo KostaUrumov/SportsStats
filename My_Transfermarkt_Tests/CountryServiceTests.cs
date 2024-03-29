63deeb80d14aad06a7ab32609b5d070aadb27e61
Kostadin Urumov <koceurumov@gmail.com>
2024-03-29 13:02:18 +0000

    some more unit tests added

--- My_Transfermarkt_Tests/CountryServiceTests.cs
+++ My_Transfermarkt_Tests/CountryServiceTests.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 using My_Transfermarkt.Data;
 using My_Transfermarkt_Core.Contracts;
+using My_Transfermarkt_Core.Models.CountryModels;
 using My_Transfermarkt_Core.Services;
 using Country = My_Transfermarkt_Infastructure.DataModels.Country;
 
@@ -22,14 +22,14 @@ namespace My_Transfermarkt_Tests
                 new Country(){Id = 2, Name= "Belgium", ShortName="BE"}
             };
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .   UseInMemoryDatabase(databaseName: "MyMockedData")
+                .UseInMemoryDatabase(databaseName: "MyMockedData")
                    .Options;
             this.data = new ApplicationDbContext(options);
             this.data.AddRange(this.countries);
             this.data.SaveChanges();
         }
 
-       
+
         [Test]
         public void TestFindByNameCorrectly()
         {
@@ -47,7 +47,7 @@ namespace My_Transfermarkt_Tests
         }
 
         [Test]
-        public void TestFindByNameNotCorrectly()
+        public void TestFindByNameNotCorrect()
         {
             var result = new Country()
             {
@@ -62,5 +62,122 @@ namespace My_Transfermarkt_Tests
 
             Assert.That(country.Result, Is.EqualTo(null));
         }
+
+        [Test]
+        public void TestFindCountryById()
+        {
+            var result = new Country()
+            {
+                Id = 1,
+                Name = "Spain",
+                ShortName = "SP"
+
+            };
+            ICountryService service = new CountryService(data);
+            var country = service.FindCountry(1);
+            Assert.That(country.Id, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestFindCountryReturningNull()
+        {
+            var result = new Country()
+            {
+                Id = 1,
+                Name = "Spain",
+                ShortName = "SP"
+
+            };
+            ICountryService service = new CountryService(data);
+            var country = service.FindCountry(10);
+            Assert.That(country.Result, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void TestIsAlreadyCreatedTrue()
+        {
+            AddNewCountryModel model = new AddNewCountryModel()
+            {
+                Id = 1,
+                Name = "Spain",
+                ShortName = "SP"
+            };
+            ICountryService service = new CountryService(data);
+            var result = service.IsAlreadyCreated(model);
+            Assert.That(result.Result.ToString(), Is.EqualTo("True"));
+        }
+
+        [Test]
+        public void TestIsAlreadyCreatedFalse()
+        {
+            AddNewCountryModel model = new AddNewCountryModel()
+            {
+                Id = 56,
+                Name = "Norway",
+                ShortName = "NO"
+            };
+            ICountryService service = new CountryService(data);
+            var result = service.IsAlreadyCreated(model);
+            Assert.That(result.Result.ToString(), Is.EqualTo("False"));
+        }
+
+        [Test]
+        public void TestIsAllCountries()
+        {
+            ICountryService service = new CountryService(data);
+            var result = service.AllCountriesAsync();
+            Assert.That(result.Result.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestIsAddCountry()
+        {
+            AddNewCountryModel model = new AddNewCountryModel()
+            {
+                Id = 56,
+                Name = "Norway",
+                ShortName = "NO"
+            };
+            ICountryService service = new CountryService(data);
+            service.AddCountryAsync(model);
+            Assert.That(data.Countries.Count(), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void TestIsFindCountryC()
+        {
+
+            ICountryService service = new CountryService(data);
+            var result = service.FindCountries("Spa");
+            var secondResult = service.FindCountries("bulf");
+
+            Assert.That(result.Result.Count, Is.EqualTo(1));
+            Assert.That(secondResult.Result.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestIsSaveChanges()
+        {
+            AddNewCountryModel model = new AddNewCountryModel()
+            {
+                Id = 1,
+                Name = "Norway",
+                ShortName = "NO"
+            };
+
+            ICountryService service = new CountryService(data);
+            service.SaveChangesAsync(model);
+            var result = service.FindCountry(1);
+            Assert.That(result.Result.Name, Is.EqualTo("Norway"));
+        }
+
+        [Test]
+        public void TestGetAllContries()
+        {
+            ICountryService service = new CountryService(data);
+            var result = service.GetAllCuntries();
+            Assert.That(result.Result.Count(), Is.EqualTo(2));
+        }
+
     }
 }
---
+++ My_Transfermarkt_Tests/StadiumServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using My_Transfermarkt.Data;
+using My_Transfermarkt_Core.Contracts;
+using My_Transfermarkt_Core.Services;
+using My_Transfermarkt_Infastructure.DataModels;
+
+namespace My_Transfermarkt_Tests
+{
+    [TestFixture]
+    public class StadiumServiceTests
+    {
+        private IEnumerable<Stadium> stadiums;
+        private ApplicationDbContext data;
+        private IEnumerable<Country> countries;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.countries = new List<Country>()
+            {
+                new Country(){Id = 1, Name= "Spain", ShortName = "SP"},
+                new Country(){Id = 2, Name= "Belgium", ShortName="BE"}
+            };
+
+            this.stadiums = new List<Stadium>()
+            {
+                new Stadium { Id = 1, CountryId = 1, Build = DateTime.Parse("1999/01/01"), Capacity = 19999, Name = "New Anfield" },
+                new Stadium { Id = 2, CountryId = 1, Build = DateTime.Parse("1899/01/01"), Capacity = 33999, Name = "Arena Koblenz" },
+                new Stadium { Id = 3, CountryId = 1, Build = DateTime.Parse("1999/01/01"), Capacity = 75024, Name = "Alianz Arena" },
+                new Stadium { Id = 4, CountryId = 2, Build = DateTime.Parse("1934/08/06"), Capacity = 74667, Name = "Olympiastadion" },
+                new Stadium { Id = 5, CountryId = 2, Build = DateTime.Parse("1969/10/06"), Capacity = 24310, Name = "Bochum Arena" },
+                new Stadium { Id = 6, CountryId = 2, Build = DateTime.Parse("1977/01/01"), Capacity = 33979, Name = "Arena Diesel" },
+                new Stadium { Id = 7, CountryId = 2, Build = DateTime.Parse("1948/01/01"), Capacity = 51024, Name = "Solna Arena" },
+            };
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "MyMockedData")
+                   .Options;
+            this.data = new ApplicationDbContext(options);
+            this.data.AddRange(this.stadiums);
+            this.data.AddRange(this.countries);
+            this.data.SaveChanges();
+        }
+
+        [Test]
+        public void TestAllAvailableStadiums()
+        {
+            IStadiumService service = new StadiumService(data);
+            var result = service.AllAvailableStadiums();
+            Assert.That(result.Result.Count(), Is.EqualTo(7));
+        }
+
+        [Test]
+        public void TestGetAllStadiums()
+        {
+            IStadiumService service = new StadiumService(data);
+            var result = service.GetAllStadiums();
+            Assert.That(result.Result.Count(), Is.EqualTo(7));
+        }
+    }
+}
